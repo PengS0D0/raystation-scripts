0222e2018d260be31154d919b1516e73278e3cfc
Dr. Z. Y. Peng <118853932+PengS0D0@users.noreply.github.com>
2024-05-29 12:21:23 -0500

    Update database.py
    my Raystation 11B is not supported pymsql but ODBC, all the mosaiq related have been modified

--- mosaiq/database.py
+++ mosaiq/database.py
@@ -6,13 +6,9 @@
 # Christoffer Lervåg
 # Helse Møre og Romsdal HF
 #
-# Python 3.6
+#raystation 11B is only support ODBC,
 
-# Used for GUI debugging:
-#from tkinter import *
-#from tkinter import messagebox
-
-import pymssql
+import pyodbc 
 
 class Database:
   
@@ -26,25 +22,50 @@ class Database:
   # Returns all rows matching the given query text (or an empty list if no match).
   @staticmethod
   def fetch_all(text):
-    conn = pymssql.connect(server=Database.server, user=Database.user, password=Database.password)
+    conn =  pyodbc.connect(driver='{SQL Server}',
+                                              host=server,
+                                              user=user, 
+                                              password=password, 
+                                              trusted_connection='no', #yes for Windows authenticationn 
+                                              database='MOSAIQ')
     cursor = conn.cursor(as_dict=True)
-    cursor.execute(text)
+    if parameters:
+      # print(text)
+      cursor.execute(text,parameters)
+    else:
+      cursor.execute(text)    
+    columns = [column[0] for column in cursor.description]
     rows = list()
     for row in cursor:
-      rows.append(row)
-      #print(str(row))
+      rows.append(dict(zip(columns,row)))
     conn.close()
     return rows
   
   # Returns a single row matching the given query text (or None if no match).
   @staticmethod
   def fetch_one(text):
-    conn = pymssql.connect(server=Database.server, user=Database.user, password=Database.password)
-    cursor = conn.cursor(as_dict=True)
-    cursor.execute(text)
-    row = cursor.fetchone()
-    #print(str(row))
+    conn = pyodbc.connect(driver='{SQL Server}',
+                                              host=server,
+                                              user=user, 
+                                              password=password, 
+                                              trusted_connection='no', #yes for Windows authenticationn 
+                                              database='MOSAIQ')
+    cursor = conn.cursor()
+    if parameters:
+      try:
+        cursor.execute(text,parameters)#return self._cursor.execute(query,parameters)
+        row = cursor.fetchone()
+      except:
+        row = None
+    else:
+      try:
+        cursor.execute(text)
+        row = cursor.fetchone()
+      except:
+        row = None #print(str(row))
     conn.close()
+    columns = [column[0] for column in cursor.description]#get column header
+    row = dict(zip(columns,row))
     return row
 
 #root = Tk()
